5c9e707116b408760a8093d0c9b20e764a9f8130
vishakha ganatra <vishu886644@gmail.com>
2020-12-12 13:02:38 +0530

    Initial commit

---
+++ js/vuebasic.js
@@ -0,0 +1,9 @@
+Vue.createApp({
+    data(){
+        return {
+            course:'it is vue js course' ,//here we use anything like string,number,object etc..
+            //vue-blind 
+            vuelink:'https://vuejs.org/'
+        };
+    }
+}).mount('#app');
