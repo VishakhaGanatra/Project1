98bb861bd0e28b18f3d8d12e84104c711f57f5b9
vishakha ganatra <vishu886644@gmail.com>
2021-01-30 12:27:45 +0530

    hi

--- datatable/src/App.vue
+++ datatable/src/App.vue
@@ -1,32 +1,14 @@
 <template>
-<div>
-<employee-new :posts="posts"></employee-new>
-
-</div>
+<Index></Index>
 </template> 
 
 
 <script>
-import EmployeeNew from './components/EmployeeNew.vue';
-import axios from 'axios';
+import Index from './components/Index.vue';
 
 export default{
   components:{
-    EmployeeNew
+    Index
   },
- data:()=>({
-  posts:[]
-  }),
-  mounted(){
-    axios.get('https://employeedetails-73030-default-rtdb.firebaseio.com/data.json').then(res=>{
-      this.posts = res.data;
-    })
-    
-  },
-  // methods:{
-  //   submitData(){
-  //     axios.post('',data).then().catch()
-  //   }
-  // }
 }
 </script>
